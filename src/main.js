453c1e67317c78cc87c3bf31207d3e99f8a06f60
Lilia <sltvu93@gmail.com>
2019-10-27 14:43:18 +0300

    27.10.2019 теперь лиля трезвая, сама коммитит

--- src/main.js
+++ src/main.js
@@ -1,15 +1,63 @@
 
 
-jQuery(function($) {
-
   $(document).ready(function () {
 
     console.log('YEP');
 
-    
+    function forRes() {
+      var wWindow = $(window).width();
+      console.log(wWindow);
+
+      if (wWindow < 768) {
+
+        $('.header-logo__burger').on('click', function () {
+
+          $('.header').toggleClass('burger-opened');
+
+          $('.header-nav, .header-social').fadeToggle(300);
+
+        });
+
+      } else {
+
+        $(window).scroll(scrollArrow);
+
+        function scrollArrow() {
+          var scrolled = $(window).scrollTop();
+
+          if (scrolled > 130){
+
+            $('.one-trouble').removeClass('active');
+            $('.double-trouble').addClass('active');
+            $('.header-logo__burger').addClass('active');
+
+            $('.header-logo__burger').on('click', function () {
+
+                $('.one-trouble').toggleClass('move-left');
+                $('.header').toggleClass('burger-opened');
+            });
+
+
+
+          } else {
+
+            $('.one-trouble').addClass('active');
+            $('.double-trouble').removeClass('active');
+            $('.header-logo__burger').removeClass('active');
+
+            $('.one-trouble').removeClass('move-left');
+
+          }
+        }
+      }
+    }
+
+    forRes();
+
+
+
 
   });
 
-});
 
 
